a68f8ef644fe53fd3db3018b6a153b4bd394f70a
user <user@Eno-ITs-MacBook-Pro.local>
2023-11-15 11:36:48 +0700

    sửa bài ex4-02

--- ex04/ex4-02.js
+++ ex04/ex4-02.js
@@ -1,8 +1,13 @@
-// 2. print how to read number in vietnamese: n integer < 1,000,000
-// 726503 : bảy mươi hai vạn sáu ngàn năm trăm linh ba.
+function readNumber(inputNumber) {
+  if (
+    inputNumber < 0 ||
+    inputNumber >= 1000000 ||
+    !Number.isInteger(inputNumber)
+  ) {
+    return 'Invalid input';
+  }
 
-const numberToVietnamese = number => {
-  const dv = [
+  const units = [
     '',
     'một',
     'hai',
@@ -15,57 +20,34 @@ const numberToVietnamese = number => {
     'chín',
   ];
 
-  const readThreeDigits = num => {
-    const tram = Math.floor(num / 100);
-    const chuc = Math.floor((num % 100) / 10);
-    const donVi = num % 10;
-    let result = '';
-
-    if (tram > 0) {
-      result += dv[tram] + ' trăm ';
-    }
-
-    if (chuc === 0 && donVi === 0 && tram > 0) {
-      result += 'linh ';
-    } else if (chuc === 0 && donVi > 0) {
-      result += 'linh ' + dv[donVi] + ' ';
-    } else if (chuc === 1) {
-      result += 'mười ' + dv[donVi] + ' ';
-    } else if (chuc > 1) {
-      result += dv[chuc] + ' mươi ';
-      if (donVi > 0) {
-        result += dv[donVi] + ' ';
-      }
-    }
-
-    return result.trim();
-  };
-
-  if (number === 0) return 'không';
-
   let result = '';
-  let index = 0;
+  let numberStr = inputNumber.toString().padStart(6, '0');
 
-  while (number > 0) {
-    const group = number % 1000;
-    if (group > 0) {
-      let read = readThreeDigits(group);
-      if (index === 1) {
-        read += ' vạn ';
-      } else if (index === 2) {
-        read += ' triệu ';
+  for (let i = 0; i < 6; i++) {
+    const digit = parseInt(numberStr[i]);
+
+    if (i === 0 && digit !== 0) {
+      result += units[digit] + ' mươi ';
+    } else if (i === 1 && digit !== 0) {
+      result += units[digit] + ' vạn ';
+    } else if (i === 2 && digit !== 0) {
+      result += units[digit] + ' ngàn ';
+    } else if (i === 3 && digit !== 0) {
+      result += units[digit] + ' trăm ';
+    } else if (i === 4) {
+      if (digit === 0) {
+        result += 'linh ';
+      } else {
+        result += units[digit] + ' mươi ';
       }
-      result = read + result;
+    } else if (i === 5 && digit !== 0) {
+      result += units[digit];
     }
-    index++;
-    number = Math.floor(number / 1000);
   }
 
   return result.trim();
-};
+}
 
-// test case:
-// const number = 726503;
-const number = 32232;
-const vietnameseNumber = numberToVietnamese(number);
-console.log(vietnameseNumber); // Output: 'bảy mươi hai vạn sáu ngàn năm trăm linh ba'
+const number = 726503;
+const vietnamese = readNumber(number);
+console.log(vietnamese);
